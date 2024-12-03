e2b0a4505c06a6976bf5a5d90d144a060b42dd49
GCGE222 <118153651+GCGE222@users.noreply.github.com>
2024-12-03 13:35:21 -0500

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 from django.core.management.utils import get_random_secret_key
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', get_random_secret_key())
-
+CSRF_TRUSTED_ORIGINS = ['https://insurancebroker.onrender.com']
 DEBUG = False
 
 ALLOWED_HOSTS = ['https://insurancebroker.onrender.com', 'localhost', '127.0.0.1']
