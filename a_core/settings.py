e5fcdd499516777eb471b79cb946cfc10ff89490
GCGE222 <118153651+GCGE222@users.noreply.github.com>
2024-12-03 13:14:01 -0500

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-5q@_my=+gray2_x918$a^!y&(hkv2%s488*qf92$m=1@_o=h14'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
